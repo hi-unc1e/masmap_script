14c7921ee9ee87a1087db715115a482b612998eb
hi-unc1e <67778054+hi-unc1e@users.noreply.github.com>
2024-04-10 14:33:28 +0800

    Update masmap_script.py
    convert

--- masmap_script.py
+++ masmap_script.py
@@ -116,6 +116,7 @@ class Nmap_Scanner:
 
 # @pysnooper.snoop()
 def split_task(ip): #split ports range in 5 pieces and return a list
+    ip = str(ip)
     if not re.match(r'\d+\.\d+\.\d+.\d+', ip):
         raise Exception("Invalid IP format: '"+ip+"'")
     ports_list = [str(x) for x in range(1,65536)]
